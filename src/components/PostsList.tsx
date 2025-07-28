22c18b6474d351c86971bf97381b2d56bfeff3e4
vincentt14 <stefanuslim844@gmail.com>
2025-07-28 08:56:31 +0700

    add new post

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,12 +1,16 @@
 import Post from "./Post";
+import NewPost from "./NewPost";
 import classes from "./PostsList.module.css";
 
 function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Vincent" body="React JS is awesome." />
-      <Post author="author 2" body="test 123" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Vincent" body="React JS is awesome." />
+        <Post author="author 2" body="test 123" />
+      </ul>
+    </>
   );
 }
 
